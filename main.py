52c10a13de750d787a162a0b35029c1f4fee3086
Vinayaka <vinayaka.bv@nighthack.in>
2024-07-14 00:39:26 +0530

    cors enabled for all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,APIRouter
 from dashboard.dashboard import dashbboard
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(debug=True)
 app_router=APIRouter()
@@ -7,3 +8,11 @@ app_router=APIRouter()
 app_router.include_router(dashbboard)
 
 app.include_router(app_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"], 
+)
