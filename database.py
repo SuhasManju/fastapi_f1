8dbb16ec34c4a0775e9d08e26762cf6e28596d3a
root <suhas200124@gmail.com>
2024-08-16 13:19:16 +0000

    database fix

--- database.py
+++ database.py
@@ -7,12 +7,7 @@ import os
 load_dotenv()
 database_url=os.getenv("database_url")
 
-<<<<<<< HEAD
-
-engine=create_engine("mysql+pymysql://root:root@127.0.0.1/formula1_new")
-=======
 engine=create_engine(database_url)
->>>>>>> ce747e1cc7fb507843c459e8296c5ae0dfd0220e
 
 sessionLocal=sessionmaker(bind=engine)
 
--- database2.py
+++ database2.py
@@ -8,17 +8,8 @@ load_dotenv()
 
 database_url=os.getenv("database_url2")
 
-<<<<<<< HEAD
-engine=create_engine("mysql+pymysql://root:root@127.0.0.1/formula1")
-
-sessionLocal=sessionmaker(bind=engine)
-
-
-Base.metadata.create_all(engine)
-=======
 engine=create_engine(database_url)
 
 sessionLocal=sessionmaker(bind=engine)
 
 Base.metadata.create_all(engine)
->>>>>>> ce747e1cc7fb507843c459e8296c5ae0dfd0220e
