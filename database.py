ce747e1cc7fb507843c459e8296c5ae0dfd0220e
SuhasManju <suhas200124@gmail.com>
2024-08-16 18:41:29 +0530

    add env file

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine,func
 from sqlalchemy.orm import sessionmaker
 from model1 import Base
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+database_url=os.getenv("database_url")
 
-
-engine=create_engine("mysql+pymysql://root:root@localhost/formula1_new")
+engine=create_engine(database_url)
 
 sessionLocal=sessionmaker(bind=engine)
-
 
 Base.metadata.create_all(engine)
--- database2.py
+++ database2.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model2 import Base
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
+database_url=os.getenv("database_url2")
 
-engine=create_engine("mysql+pymysql://root:root@localhost/formula1")
+engine=create_engine(database_url)
 
 sessionLocal=sessionmaker(bind=engine)
-
 
 Base.metadata.create_all(engine)
